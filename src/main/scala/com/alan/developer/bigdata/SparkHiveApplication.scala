1786fd493d732787357d963948f79ece01a62595
Alan <ustargab@gmail.com>
2018-10-03 21:46:27 +0200

    spark 1.6.0 java 1.7 scala 2.10.5

--- src/main/scala/com/alan/developer/bigdata/SparkHiveApplication.scala
+++ src/main/scala/com/alan/developer/bigdata/SparkHiveApplication.scala
@@ -1,22 +1,28 @@
 package com.alan.developer.bigdata
 
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.{Row, SQLContext}
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.hive.HiveContext
 
+/**
+  * DML hive tables from spark.
+  */
 object SparkHiveApplication {
 
   def main(args: Array[String]): Unit = {
     //val sc = new SparkContext(new SparkConf().setAppName("Spark Hive").setMaster("local[*]"))
     val sc = SparkContext.getOrCreate()
-    val sqlContext = new SQLContext(sc)
-    val fic = sc.textFile("/bigdata/retail/online-retail.txt")
+    val sqlContext = new HiveContext(sc)
+    val fic = sc.textFile(args(0))
     val df_retail = fic.map(_.split(";").toSeq).map(x => Row(x(0), x(1), x(2), Option(x(3)).getOrElse("0").toInt,
       x(4), Option(x(5)).getOrElse("0.0").replace(",", ".").toFloat, Option(x(6))
         .map(d => if (d.isEmpty) "0" else d).getOrElse("0").toLong, x(7)))
-    val sc_retail = sqlContext.table("bigdata.retail").schema
+    val sc_retail = sqlContext.table(args(1)).schema
     val df = sqlContext.createDataFrame(df_retail, sc_retail)
-    df.write.mode("overwrite").insertInto("bigdata.retail")
-    println(s"Total records ${df count}")
+    df.write.mode("overwrite").insertInto(args(1))
+    println("***************************")
+    println(s"It has inserted ${df count} records")
+    println("***************************")
   }
 }
 
