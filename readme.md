e49a873438538884363146477ef265457e0567c8
Alan <ustargab@gmail.com>
2018-10-10 07:40:51 +0200

    Central logging Documentación

--- readme.md
+++ readme.md
@@ -3,6 +3,73 @@ We use shade plugin for packaging all mandatory dependencies.
 ```
 spark-submit --conf spark.es.nodes=elasticsearch --class com.alan.developer.bigdata.AppicationLogging "logging-streaming-kafka-1.0-SNAPSHOT.jar"
 ```
+We get a sample of records from ES with
+```
+http://localhost:9200/logging-*/_search
+```
+Mapping for indexes in ES
+```
+http://localhost:9200/_template/logging
+{
+  "index_patterns": [
+    "logging-*"
+  ],
+  "settings": {
+    "number_of_shards": 1
+  },
+  "mappings": {
+    "log": {
+      "_source": {
+        "enabled": true
+      },
+      "properties": {
+        "appName": {
+          "type": "keyword",
+          "fields": {
+            "keyword": {
+              "type": "keyword",
+              "ignore_above": 256
+            }
+          }
+        },
+        "clazz": {
+          "type": "text",
+          "fields": {
+            "keyword": {
+              "type": "keyword",
+              "ignore_above": 256
+            }
+          }
+        },
+        "level": {
+          "type": "text",
+          "fields": {
+            "keyword": {
+              "type": "keyword",
+              "ignore_above": 256
+            }
+          }
+        },
+        "message": {
+          "type": "text",
+          "fields": {
+            "keyword": {
+              "type": "keyword",
+              "ignore_above": 256
+            }
+          }
+        },
+        "timestamp": {
+          "type": "date",
+          "format": "yyyy-MM-dd HH:mm:ss"
+        }
+      }
+    }
+  }
+}
+```
+
+
 # Creates topic
 ```
 kafka-topics.bat --create --topic logs-app --partitions 1 --replication-factor 1 --zookeeper kafka:2181
--- src/main/scala/com/alan/developer/bigdata/CentralLogging.scala
+++ src/main/scala/com/alan/developer/bigdata/CentralLogging.scala
@@ -5,15 +5,16 @@ import java.time.format.DateTimeFormatter
 
 import kafka.serializer.{DefaultDecoder, StringDecoder}
 import org.apache.spark.SparkConf
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.storage.StorageLevel
-import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.elasticsearch.spark._
 import org.slf4j.{Logger, LoggerFactory}
 
+/**
+  * Central de procesado de logs.
+  */
 object CentralLogging {
 
   val log: Logger = LoggerFactory.getLogger(getClass)
@@ -23,32 +24,29 @@ object CentralLogging {
 
     val spark = SparkSession.builder.config(new SparkConf().setAppName("Central Loggin App").setMaster("local[*]"))
       .getOrCreate()
+    @transient
     val sc = spark.sparkContext
     val ssc = new StreamingContext(sc, Seconds(5))
     val params: Map[String, String] = Map(
-      "metadata.broker.list" -> "localhost:9092",
-      "zookeeper.connect" -> "localhost:2181",
+      "metadata.broker.list" -> args(0),
+      "zookeeper.connect" -> args(1),
       "group.id" -> "spark-streaming",
       "zookeeper.connection.timeout.ms" -> "5000"
     )
     val topics = List("logging")
     val kafkaStream = KafkaUtils.createStream[Array[Byte], String, DefaultDecoder, StringDecoder](ssc, params,
       topics.map((_, 1)).toMap, StorageLevel.MEMORY_ONLY_SER).map(_._2)
-    val index = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHH")).concat("/log")
+    val index = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHH"))
     val allRdd = kafkaStream.map(linea => linea.split("\\|").toSeq)
       .filter(_.length == 5)
-      .map(x => (x(0), GenericLog(x(0),
-        LocalDateTime.parse(x(1), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")),
-        x(2), x(3), x(4))))
-    val grouped = allRdd.groupByKey.map(pairs => (pairs._1, sc.parallelize(pairs._2.toSeq)))
-
-    grouped.foreachRDD(group => group.foreach(rdd => rdd._2.saveToEs("logger-".concat(rdd._1).concat(index))))
-
-    //.foreachRDD(rdd => rdd..saveToEs(index))
+      .map(x => GenericLog(x(0).toLowerCase,
+        x(1),
+        x(2).trim, x(3), x(4)))
+    allRdd.foreachRDD(rdd => rdd.saveToEs("logging-{appName}" + index + "/log"))
 
     ssc.start()
     ssc.awaitTermination()
   }
 }
 
-case class GenericLog(appName: String, timestamp: LocalDateTime, level: String, clazz: String, message: String)
+case class GenericLog(appName: String, timestamp: String, level: String, clazz: String, message: String)
