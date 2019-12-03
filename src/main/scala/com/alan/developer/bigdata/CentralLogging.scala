40c7732a83d087a1c9c5f1d6d726e48338ec44eb
ustargab@gmail.com <b24c2bd99ad04a7e98daa526aa4be475>
2019-12-03 17:21:46 +0100

    Now elasticsearch mapping is without type

--- src/main/scala/com/alan/developer/bigdata/CentralLogging.scala
+++ src/main/scala/com/alan/developer/bigdata/CentralLogging.scala
@@ -1,8 +1,10 @@
 package com.alan.developer.bigdata
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 import com.alan.developer.bigdata.model.{JaegerLog, JaegerProcess, Reference, Tag}
 import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
@@ -32,24 +34,26 @@ object CentralLogging {
     //val spark = SparkSession.builder.config(new SparkConf().setAppName("Central Loggin App").setMaster("local[*]").set("es.index.auto.create", "true")).getOrCreate()
     val spark = SparkSession.builder.appName("Jaeger processing").getOrCreate()
     val sc = spark.sparkContext
-    val ssc = new StreamingContext(sc, Seconds(5))
+    val ssc = new StreamingContext(sc, Seconds(Option(args(1).toLong).getOrElse(5L)))
     val stream = KafkaUtils.createDirectStream[String, String](
       ssc,
       PreferConsistent,
       Subscribe[String, String](topics, kafkaParams)
     )
+    val index = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
     stream.map(record => {
       import spray.json._
       import DefaultJsonProtocol._
       implicit val tag = jsonFormat2(Tag)
       implicit val process = jsonFormat2(JaegerProcess)
       implicit val reference = jsonFormat2(Reference)
-      implicit val log = jsonFormat9(JaegerLog)
+      implicit val log = jsonFormat11(JaegerLog)
 
       record.value.parseJson.convertTo[JaegerLog]
     })
-      .foreachRDD(rdd => rdd.saveToEs("jaeger-v1-{process.serviceName}-{@timestamp|yyyy.MM.dd}")
-    )
+      .map(r => r.copy(processDate = Option(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))),
+        startTime = LocalDateTime.parse(r.startTime, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))))
+      .foreachRDD(rdd => rdd.saveToEs("jaeger-v1-{process.serviceName}-" concat index))
     ssc.start()
     ssc.awaitTermination()
   }
--- src/main/scala/com/alan/developer/bigdata/model/JaegerLog.scala
+++ src/main/scala/com/alan/developer/bigdata/model/JaegerLog.scala
@@ -1,11 +1,17 @@
 package com.alan.developer.bigdata.model
 
-import spray.json.{DefaultJsonProtocol, JsNumber, JsValue, JsonFormat}
+import java.time.LocalDateTime
+
+import spray.json.{JsNumber, JsValue, JsonFormat}
 
 case class JaegerLog(traceId: String, spanId: String, operationName: String, references: Option[Array[Reference]],
-                     flags: Int, startTime: String, duration: String , tags: Array[Tag], process: JaegerProcess)
+                     flags: Int, startTime: String, duration: String, tags: Array[Tag], process: JaegerProcess,
+                     processDate: Option[String], ingestDate: Option[String])
+
 case class Reference(traceId: String, spanId: String)
+
 case class Tag(key: String, vStr: Option[String])
+
 case class JaegerProcess(serviceName: String, tags: Array[Tag])
 
 object FloatJsonFormat extends JsonFormat[Float] {
